5db0d0e9d840f8f5af32c0a87cfe7674fe254829
ta3nich <118207850+ta3nich@users.noreply.github.com>
2022-12-17 06:49:13 -0600

    Create ng.sh

---
+++ payload/ng.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+### every exit != 0 fails the script
+
+
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
+tar xvf ngrok-stable-linux-amd64.tgz && chmod +x ngrok && cp ngrok /usr/bin/
+git clone https://github.com/GH0STAV0/SDA_ALL.git
+cp /root/install_add/geckodriver22 /usr/bin/
+export NO_VNC_HOME=/usr/share/novnc
+rm /headless/*.bz2*
+
+git clone https://github.com/GH0STAV0/ALL_HCL.git
+service tor restart
+apt-get autoremove --yes
+apt-get autoclean 
+apt-get autoremove --yes
+rm -rf /var/lib/apt/lists/*
+rm -rf /var/lib/{apt,dpkg,cache,log}/
