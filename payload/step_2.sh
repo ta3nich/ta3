1dd97bec71b76b058c1ae5a46597d4fce7e9ee0a
ta3nich <118207850+ta3nich@users.noreply.github.com>
2022-12-17 17:55:07 -0600

    Update step_2.sh
    dbus

--- payload/step_2.sh
+++ payload/step_2.sh
@@ -17,7 +17,11 @@ apt install --assume-yes ./chrome-remote-desktop_current_amd64.deb
 apt install --assume-yes ./google-chrome-stable_current_amd64.deb
 adduser azer chrome-remote-desktop
 DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AWgavderlsPEPkuGg1tVHX7LWh9qB_d9gaOKvHil56EE-4QJ1_SoJBToV5Khe01x2eQECQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --pin=001122
+wget -O ~/wall.jpg https://w.wallhaven.cc/full/9m/wallhaven-9m7l2k.jpg
+xfconf-query --create xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s ~/wall.jpg
 
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s ~/wall.jpg
+export $(dbus-launch)
 
 
 rm *.tar.bz2 *.tar.gz
