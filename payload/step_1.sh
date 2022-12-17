f99025d2300e5e476425496f80aa7ce2bd0b88c7
ta3nich <118207850+ta3nich@users.noreply.github.com>
2022-12-17 09:25:41 -0600

    Create step_1.sh

---
+++ payload/step_1.sh
@@ -0,0 +1,19 @@
+apt-get -f install  -y \
+        jq \
+        nano \
+        psmisc \
+        sudo \
+        cron python3-pil \
+        software-properties-common python3 python3-dev python3-numpy \
+        openvpn autocutsel  python3-pip python3-tk python3-dev  pwgen  \
+        wget openssh-server locate nano gedit screen  net-tools curl git tor \
+        xfce4-goodies xfce4\
+        thunar tumbler xarchiver \
+        mugshot thunar-archive-plugin \
+        dbus-x11 xauth xinit x11-xserver-utils xdg-utils \
+        tightvncserver novnc python3-websockify python3-pyvirtualdisplay \
+        build-essential \
+        xvfb xserver-xephyr ttf-wqy-zenhei python2.7
+pip3 install selenium==4.0.0.a1 pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4   ConfigParser lxml  speechrecognition requests \
+ pyvirtualdisplay pydub pyautogui emojis emoji  \
+ mysql-connector-python device_detector selenium-stealth
