52a893fd96cd3f8987509c5fe7ba3b3094badee8
ta3nich <118207850+ta3nich@users.noreply.github.com>
2022-12-17 10:23:44 -0600

    Update step_2.sh
    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
    wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb

--- payload/step_2.sh
+++ payload/step_2.sh
@@ -11,10 +11,12 @@ wget -q https://ftp.mozilla.org/pub/firefox/releases/60.0.1esr/linux-x86_64/en-U
 echo " * 3 ------------- > INSTALL geckodriver-v0.32.0 "
 wget https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz
 
-apt install --assume-yes xfce4 desktop-base xfce4-terminal
+apt install --assume-yes xfce4 desktop-base xfce4-terminal xscreensaver
 
 bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'
 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
 
 
 
