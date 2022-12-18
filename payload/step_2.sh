95ee09a02ea1d55a9d6f80172b8eab2c9de7d8e2
ta3nich <118207850+ta3nich@users.noreply.github.com>
2022-12-18 14:28:30 -0600

    Update step_2.sh

--- payload/step_2.sh
+++ payload/step_2.sh
@@ -23,8 +23,9 @@ apt --assume-yes autoremove
 apt install --assume-yes ./chrome-remote-desktop_current_amd64.deb
 apt install --assume-yes ./google-chrome-stable_current_amd64.deb
 #ocs-url_3.1.0-0ubuntu1_amd64.deb
-apt install --assume-yes ./adduser azer chrome-remote-desktop
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AWgavderlsPEPkuGg1tVHX7LWh9qB_d9gaOKvHil56EE-4QJ1_SoJBToV5Khe01x2eQECQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --pin=001122
+#apt install --assume-yes ./
+#adduser azer chrome-remote-desktop
+#DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AWgavderlsPEPkuGg1tVHX7LWh9qB_d9gaOKvHil56EE-4QJ1_SoJBToV5Khe01x2eQECQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --pin=001122
 wget -O ~/wall.jpg https://w.wallhaven.cc/full/9m/wallhaven-9m7l2k.jpg
 xfconf-query --create xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s ~/wall.jpg
 
