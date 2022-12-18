d59fdb0ad75b9a02f97a5f6be0876ef431f995c5
ta3nich <118207850+ta3nich@users.noreply.github.com>
2022-12-17 18:44:56 -0600

    Update step_2.sh

--- payload/step_2.sh
+++ payload/step_2.sh
@@ -13,9 +13,13 @@ echo $username':'$password | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 mkdir -p /home/azer/.config
 chown azer:azer /home/azer/.config
+apt install --assume-yes xfce4 desktop-base xfce4-terminal nano
+apt remove --assume-yes gnome-terminal
+apt --assume-yes autoremove
 apt install --assume-yes ./chrome-remote-desktop_current_amd64.deb
 apt install --assume-yes ./google-chrome-stable_current_amd64.deb
-adduser azer chrome-remote-desktop
+#ocs-url_3.1.0-0ubuntu1_amd64.deb
+apt install --assume-yes ./adduser azer chrome-remote-desktop
 DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AWgavderlsPEPkuGg1tVHX7LWh9qB_d9gaOKvHil56EE-4QJ1_SoJBToV5Khe01x2eQECQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --pin=001122
 wget -O ~/wall.jpg https://w.wallhaven.cc/full/9m/wallhaven-9m7l2k.jpg
 xfconf-query --create xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s ~/wall.jpg
